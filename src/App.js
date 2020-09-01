ae25116d05dd8372790bc93f5fd047851e66c502
minkolee <minkolee@gmail.com>
2020-09-01 21:06:01 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import ControlPanel from "./component/ControlPanel";
+
+function App() {
+  return (
+      <ControlPanel />
+  );
+}
+
+export default App;
